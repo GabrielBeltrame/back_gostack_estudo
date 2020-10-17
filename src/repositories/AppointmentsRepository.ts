6f3589d8ab9fe0b80485ed7481c83624ffa3b719
Gabriel Beltrame Silva <silvagabrielbeltrame@gmail.com>
2020-10-17 15:01:11 -0300

    models and repositories -> SoC and DTO

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,33 @@
+import { Appointment } from '../models/Appointment';
+import { isEqual } from 'date-fns';
+
+interface CreateAppointmentDTO {
+    provider: string;
+    date: Date;
+}
+
+export class AppointmentsRepository {
+
+    private appointments: Array<Appointment>;
+
+    constructor() {
+        this.appointments = [];
+    }
+
+    public all(): Appointment[] {
+        return this.appointments;
+    }
+
+    public create({ provider, date }: CreateAppointmentDTO): Appointment {
+
+        const appointment: Appointment = new Appointment({provider, date})
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+
+    public findByDate(date: Date): Appointment | null {
+        return this.appointments.find(appointment => isEqual(date, appointment.date)) || null
+    }
+}
